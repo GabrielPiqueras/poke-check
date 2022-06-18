545fda49278cf2b6d29c70f2d782e89b1d9b65a7
Gabriel <rocamora_gabriel@hotmail.com>
2022-06-18 20:24:18 +0200

    Instalación Material, fuentes e iconos.

--- src/PokeCheck.js
+++ src/PokeCheck.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Button from '@mui/material/Button'
 
 export const PokeCheck = () => {
-  return (
-    <div>
-        <h1>PokeCheck</h1>
-    </div>
-  )
+    return (
+        <div>
+            <Button variant="contained">Hello World</Button>
+            <h1>PokeCheck</h1>
+        </div>
+    )
 }
