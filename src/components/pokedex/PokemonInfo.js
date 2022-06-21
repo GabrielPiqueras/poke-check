6ba72e86748aa5aecc4729a366732fbec333a2de
gabriel_inf24 <gabriel@informatica24.com>
2022-06-21 11:40:04 +0200

    Loading y css al cargar la información del pokemon.

--- src/components/pokedex/PokemonInfo.js
+++ src/components/pokedex/PokemonInfo.js
@@ -1,33 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getPokemonByName } from '../../helpers/getPokemonByName';
+import { useFetch } from '../../hooks/useFetch';
+
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 export const PokemonInfo = () => {
 
     // Obtengo el nombre
     const { name } = useParams();
 
-    // Declaro estado
-    const [ pokemon, setPokemon ] = useState({});
-    const { height } = pokemon;
+    const { data, loading, error } = useFetch(`https://pokeapi.co/api/v2/pokemon/${encodeURI(name)}/`);
+    const { height } = !!data && data;
+    console.log('loading', loading);
+    console.log('error', error);
 
-    useEffect(() => {
-      name && getPokemonByName(name).then(pokemon => setPokemon(pokemon));
-    }, [ name ]);
+    // useEffect(() => {
+    // //   name && getPokemonByName(name).then(pokemon => setPokemon(pokemon));
+    // }, [ name ]);
 
     return (
-        <div>
-            {
-                (Object.keys(pokemon).length > 0)
-                ?
-                    (<div>
-                        <h1>{ name }</h1>
-                        <h2>Altura: { height }</h2>
-                    </div>)
-                :
-                    <h2>No encontrado</h2>
-
-            }
-        </div>
+            <Box id='box-pokemon-info' sx={{ display: 'flex', alignItems: 'stretch', height: '100%' }}>
+                
+                {
+                    (loading)
+                    ?
+                        <Box id='loading-box' sx={{ display: 'flex', width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+                            <CircularProgress disableShrink size={ 120 } />
+                        </Box>
+                    :
+                        (!error)
+                        ?
+                            <Box>
+                                <h1>{ name }</h1>
+                                <h2>Altura: { height }</h2>
+                            </Box>
+                        :
+                            <p>Error</p>
+                }
+            </Box>
     )
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from "react"
 export const useFetch = (url) => {
 
     const isMounted = useRef(true);
-    const [state, setState] = useState({data: null, loading: true, error: null});
+    const [state, setState] = useState({data: null, loading: true, error: false});
     
     useEffect(() => {
         // No hace nada cuando está montado
@@ -17,7 +17,7 @@ export const useFetch = (url) => {
     useEffect(() => {
 
         // Reinicio para poder cargar el loading de nuevo
-        setState({ data: null, loading: true, error: null});
+        setState({ data: null, loading: true, error: false});
 
         const request = fetch(url)
                             .then(resp => resp.json())
@@ -27,11 +27,18 @@ export const useFetch = (url) => {
                                     setState({
                                         data: data,
                                         loading: false,
-                                        error: null
+                                        error: false
                                     });
                                 } else {
                                     console.log('No se alcanzó a llamar a setState, porque el componente fue desmontado :)')
                                 }
+                            })
+                            .catch(err => {
+                                setState({
+                                    ...state,
+                                    loading: false,
+                                    error: true
+                                })
                             });
     }, [url]);
 
